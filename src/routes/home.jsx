58a88f9e564b8f6994f7b4373bc3aee0638c14b9
Rinpyre <thecutefireboy@gmail.com>
2026-02-26 08:24:57 +0100

    refactor(homepage): Refactor HomePage component to include Navbar, Footer, and section components

--- src/routes/home.jsx
+++ src/routes/home.jsx
@@ -1,8 +1,22 @@
-export const HomePage = () => {
-    return (
-        <div className="home-page">
-            <h1 className="text-snow text-2xl font-bold">Welcome to Adrian&apos;s Portfolio</h1>
-            {/* Add library content here */}
-        </div>
-    )
-}
+﻿import { Navbar, Footer } from '@components/layout'
+import {
+    HeroSection,
+    AboutSection,
+    ProjectsSection,
+    ExperienceSection,
+    ContactSection
+} from '@components/features'
+
+export const HomePage = () => (
+    <div className="w-full">
+        <Navbar />
+        <main>
+            <HeroSection />
+            <AboutSection />
+            <ProjectsSection />
+            <ExperienceSection />
+            <ContactSection />
+        </main>
+        <Footer />
+    </div>
+)
