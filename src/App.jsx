a6df08c744c5bb8585ccf812c00c9a5d21d8c8d8
Rinpyre <thecutefireboy@gmail.com>
2026-02-26 08:23:32 +0100

    refactor(App): Simplify layout structure by removing unnecessary div wrappers

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,11 @@ import { HomePage, NotFoundPage } from '@routes'
 function App() {
     return (
         <BrowserRouter>
-            <div className="app bg-primary flex h-screen w-full">
-                <div className="flex w-full grow items-center justify-center overflow-hidden">
-                    <Routes>
-                        <Route path="/" element={<HomePage />} />
-                        <Route path="*" element={<NotFoundPage />} />
-                    </Routes>
-                </div>
+            <div className="app bg-primary min-h-screen w-full">
+                <Routes>
+                    <Route path="/" element={<HomePage />} />
+                    <Route path="*" element={<NotFoundPage />} />
+                </Routes>
             </div>
         </BrowserRouter>
     )
